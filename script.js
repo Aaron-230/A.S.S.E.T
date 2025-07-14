79ec1844e628db31a73ccab864380ff62c525951
Aaron Mathew <aaron.nadar@hotmail.com>
2025-07-14 13:46:04 +0530

    modified:   script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import { GoogleGenAI } from 'npm:@google/genai';
 
-const genAI = new GoogleGenAI({apiKey: ""});
+const genAI = new GoogleGenAI({apiKey: "AIzaSyDrBm13b7t1MmbyG5p-yqQXjv-bACehMdU"});
 
 async function generateResponse(content) {
     const response = genAI.models.generateContent({ model: "gemini-1.5-flash", content: content,});
