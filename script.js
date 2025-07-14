b061afcd9509774592b3bcb73a736ca2610f4403
Aaron Mathew <aaron.nadar@hotmail.com>
2025-07-14 13:52:50 +0530

    modified:   script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-import { GoogleGenAI } from 'npm:@google/genai';
+import { GoogleGenAI } from "@google/genai";
 
-const genAI = new GoogleGenAI({apiKey: "AIzaSyDrBm13b7t1MmbyG5p-yqQXjv-bACehMdU"});
+const ai = new GoogleGenAI({ apiKey: "AIzaSyDrBm13b7t1MmbyG5p-yqQXjv-bACehMdU" });
 
-async function generateResponse() {
-    const response = await genAI.models.generateContent({ 
-        model: "gemini-1.5-flash", 
-        content: "Explain how AI works in a few words.",
-    });
-
-    console.log(response.text);
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: "Explain how AI works in a few words",
+  });
+  console.log(response.text);
 }
 
-generateResponse();
+await main();
